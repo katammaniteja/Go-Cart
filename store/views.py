f1d3cf80ea524688409189e777d8a0bd84d97e73
Maniteja <78750315+katammaniteja@users.noreply.github.com>
2021-12-25 13:41:57 +0530

    Added

--- store/views.py
+++ store/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Categories,Products
 
 # Create your views here.
 def home(request):
     return render(request,'store/index.html');
+
+def collections(request):
+    category=Categories.objects.filter(status=0)
+    context={'category':category}
+    return render(request, 'store/collections.html',context)
