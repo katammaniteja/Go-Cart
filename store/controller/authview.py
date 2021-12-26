55c9a45817fabf6a0c941c29ac294e056aa882cb
Maniteja <78750315+katammaniteja@users.noreply.github.com>
2021-12-26 11:14:38 +0530

    Changes Added

---
+++ store/controller/authview.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render,HttpResponseRedirect,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.urls import reverse
+from store.forms import customForm
+from django.contrib import messages
+def register(request):
+    form=customForm()
+    if request.method=='POST':
+        form=customForm(request.POST)
+        if form.is_valid():
+            form.save();
+            messages.success(request,"Registered Successfully! Login to continue")
+            return redirect('/login')
+    context={"form":form}
+    return render(request, "store/auth/register.html",context)
+
+def loginpage(request):
+    if request.user.is_authenticated:
+        messages.warning(request,"You are already logged in")
+        return redirect('/')
+    else:
+        if request.method=='POST':
+            name=request.POST.get('username')
+            password=request.POST.get('password')
+
+            user=authenticate(request,username=name,password=password)
+            if user is not None:
+                login(request,user)
+                messages.success(request,"Logged in successfully")
+                return redirect("/")
+            else:
+                messages.success(request,"Invalid Username or Password")
+                return redirect('/login')
+        return render(request,'store/auth/login.html')
+
+def logoutpage(request):
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request,"Logged out successfully")
+    return redirect('/')
--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
 
+from store.controller import authview
+
 urlpatterns=[
     path('',views.home,name='home'),
     path('collections',views.collections,name='collections'),
     path('collections/<str:slug>',views.collectionsView,name="collectionsview"),
     path('collections/<str:cate_slug>/<str:pro_slug>',views.productView,name="productView"),
+
+    path('register/',authview.register,name='register'),
+    path('login/',authview.loginpage,name='loginpage'),
+    path('logout/',authview.logoutpage,name='logoutpage')
 ]
