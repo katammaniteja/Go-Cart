9ac8a477ed3646362f17c09a3573263f0666ed99
Maniteja <78750315+katammaniteja@users.noreply.github.com>
2021-12-31 12:22:40 +0530

    changes added

--- store/forms.py
+++ store/forms.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.forms import UserCreationForm
-
 from django.contrib.auth.models import User
 from django import forms
 
@@ -11,3 +10,10 @@ class customForm(UserCreationForm):
     class Meta:
         model=User
         fields=['username','email','password1','password2']
+
+    def clean(self):
+        all_cleaned_data=super().clean()
+        user_email=all_cleaned_data['email']
+        if User.objects.filter(email=user_email).exists():
+            msg="Email Already exists!"
+            self.add_error('email', msg)
