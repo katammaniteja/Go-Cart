5100fc99c85ace60591f9eda958c463892322d07
Maniteja <78750315+katammaniteja@users.noreply.github.com>
2021-12-31 17:08:13 +0530

    Update checkout.js

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
         var state=$("[name='state']").val()
         var pincode=$("[name='pincode']").val()
         var country=$("[name='country']").val()
-
+        var token = $('input[name=csrfmiddlewaretoken]').val();
 
         if(fname=="" ||lname=="" ||email=="" ||phone=="" ||address=="" ||city=="" ||state=="" ||pincode=="" ||country=="" ){
             swal("Alert", "All fileds are mandatory", "error");
@@ -23,28 +23,40 @@ $(document).ready(function(){
                 method:"GET",
                 url:"/proceed-to-pay",
                 success:function(response){
-                   // console.log(response);
                     var options = {
-                        "key": "rzp_test_dmibPrjPhWqXSd", // Enter the Key ID generated from the Dashboard
-                        "amount": response.total_price*100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+                        "key": "rzp_test_dmibPrjPhWqXSd",
+                        "amount": response.total_price*100, 
                         "currency": "INR",
                         "name": "Maniteja",
                         "description": "Thanks for shopping",
                         "image": "https://example.com/your_logo",
-                        // "order_id": "order_9A33XWu170gUtm", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-                        "handler": function (response){
-                            alert(response.razorpay_payment_id);
-                            // alert(response.razorpay_order_id);
-                            // alert(response.razorpay_signature)
+                        "handler": function (response2){
+                            alert(response2.razorpay_payment_id);
+                            data={
+                                "fname":fname,
+                                "lname":lname,
+                                "email":email,
+                                "phone":phone,
+                                "address":address,
+                                "city":city,
+                                "state":state,
+                                "pincode":pincode,
+                                "country":country,
+                                "payment_mode":"Razorpay",
+                                "payment_id":response2.razorpay_payment_id
+                            }
+                            $.ajax({
+                                method:"POST",
+                                url:"/place-order",
+                                csrfmiddlewaretoken: token,
+                                data:data,
+                            });
                         },
                         "prefill": {
                             "name": fname+" "+lname,
                             "email": email,
                             "contact": phone
                         },
-                        // "notes": {
-                        //     "address": "Razorpay Corporate Office"
-                        // },
                         "theme": {
                             "color": "#3399cc"
                         }
@@ -53,8 +65,6 @@ $(document).ready(function(){
                     rzp1.open();
                 }
             });
-
-
             
         }
     })
