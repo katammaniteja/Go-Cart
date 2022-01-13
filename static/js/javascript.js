1fd80c56863008f4b5112a6455b2fc7543c92c2b
Maniteja <78750315+katammaniteja@users.noreply.github.com>
2022-01-13 09:14:36 +0530

    updated

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -2,3 +2,15 @@ var btns=document.getElementsByClassName("btn");
 Array.from(btns).forEach((element)=>{
     element.classList.add("shadow");
 });
+
+// Start of Tawk.to Script
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/61dc6d39f7cf527e84d16538/1fp2glbgp';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+// End of Tawk.to Script
