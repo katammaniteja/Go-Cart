ac4543c8f42857c18baaf9c575dd3db7a4e3bb2f
Maniteja <78750315+katammaniteja@users.noreply.github.com>
2022-01-25 11:51:06 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -79,10 +79,6 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dc5r3ul7783vsi',
