c9af1e9154acbb1ea4a960a5ce5c6f8b035dcc05
unknown <katammaniteja@gmail.com>
2022-07-01 21:51:03 +0530

    updated tawk.to

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -3,14 +3,14 @@ Array.from(btns).forEach((element)=>{
     element.classList.add("shadow");
 });
 
-// Start of Tawk.to Script
+// <!--Start of Tawk.to Script-->
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/61dc6d39f7cf527e84d16538/1fp2glbgp';
+s1.src='https://embed.tawk.to/62bf1d9ab0d10b6f3e7a59d4/1g6t8n9n1';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
-// End of Tawk.to Script
+// <!--End of Tawk.to Script-->
